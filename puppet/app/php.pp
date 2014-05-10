e04cb0e4caa94ba71990b15b1554f6899333255e
david.papp <david.papp@fandepay.com>
2014-05-10 15:08:58 +0200

    PHP Xdebug support

--- puppet/app/php.pp
+++ puppet/app/php.pp
@@ -1,12 +1,22 @@
 class app::php {
-    package {["php5", "php5-cli", "php5-dev", "php-apc", "php5-mysql", "php5-intl", "php5-curl"]:
+  
+  case $::osfamily {
+    Redhat: {
+        $php_package = ["php", "php-dev", "php-mysql", "php-intl", "php-curl", "php-xdebug"]
+    }
+    Debian: {
+        $php_package = ["php5", "php5-cli", "php5-dev", "php5-mysql", "php5-intl", "php5-curl", "php5-xdebug"]
+    }
+  }
+  
+    package { $php_package:
         ensure => present,
         notify => Service[$webserverService],
     }
 
     exec {"clear-symfony-cache":
         require => Package["php5-cli"],
-        command => "/bin/bash -c 'cd /var/www/vhosts/$vhost.$domain && /usr/bin/php app/console cache:clear --env=dev && /usr/bin/php app/console cache:clear --env=prod'",
+        command => "/bin/bash -c 'cd $vhostpath/$vhost.$domain && /usr/bin/php app/console cache:clear --env=dev && /usr/bin/php app/console cache:clear --env=prod'",
     }
 
     if 'nginx' == $webserver {
